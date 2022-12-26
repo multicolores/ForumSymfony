32b90e6f5ab9b0a09c3a2bac1e107f434263ca08
Florian <florian.tellier02@gmail.com>
2022-12-26 13:23:29 +0100

    [Task:1.9] - Update done

--- src/Controller/DiscussionController.php
+++ src/Controller/DiscussionController.php
@@ -121,4 +121,26 @@ class DiscussionController extends AbstractController
 
         return $this->redirectToRoute('discussion', ['themeId' => $themeId]);
     }
+
+    /**
+     * @Route("/admin/edit-discussion/{discussionId}-{discussionText}", name="edit_discussion")
+     */
+    public function edit_discussion($discussionId, $discussionText, ManagerRegistry $doctrine): Response
+    {
+
+        $entityManager = $doctrine->getManager();
+        $discussionToEdit = $entityManager->getRepository(Discussion::class)->find($discussionId);
+        $themeId = $discussionToEdit->getTheme()->getId();
+
+        if (!$discussionToEdit) {
+            throw $this->createNotFoundException(
+                'Erreur, aucune discussion de correspond'
+            );
+        }
+
+        $discussionToEdit->setText($discussionText);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('discussion', ['themeId' => $themeId]);
+    }
 }
